0020f525cdad14178727bb5faa099760bd7d3124
Asif Zubair <asif.zubair@gmail.com>
2017-04-09 11:58:20 -0700

    Update generate_page_rank.py

--- solr/generate_page_rank.py
+++ solr/generate_page_rank.py
@@ -7,6 +7,6 @@ pr = nx.pagerank(G, alpha = 0.85, personalization = None, max_iter = 30,
 	tol = 1e-06, nstart = None, weight = 'weight', dangling = None)
 
 base = "/Users/asifzubair/projects/information_retrieval/solr/data/NBCNewsData/NBCNewsDownloadData/"
-with open('external_PageRankFile.txt', 'w') as out_file:
+with open('external_pageRankFile.txt', 'w') as out_file:
 	for key, value in pr.items():
 		out_file.write(os.path.join(base, key) + "=" + str(value) + "\n")
